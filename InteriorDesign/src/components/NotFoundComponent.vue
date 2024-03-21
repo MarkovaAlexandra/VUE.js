6abfc174b6482d93bd9aba7025eb9a7f75301eb5
Alexandra <alexandramarkova573@gmail.com>
2024-03-21 16:23:21 +0300

    404

--- InteriorDesign/src/components/NotFoundComponent.vue
+++ InteriorDesign/src/components/NotFoundComponent.vue
@@ -1,6 +1,19 @@
 <template>
     <div>
-        <h2>404</h2>
-        <h3>Page not found</h3>
+        <div class="notfound center">
+            <h2>404</h2>
+            <h3>Page not found</h3>
+        </div>
     </div>
 </template>
+
+<style>
+.notfound {
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+}
+</style>
